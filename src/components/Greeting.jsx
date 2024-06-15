12a4d4394df571e0c9a24dd6236aefa23eba1621
Miguel <migueldelcorso@gmail.com>
2024-06-15 20:29:51 -0300

    added dark mode

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -5,9 +5,9 @@ const Greeting = ({messages}) => {
 
     const [greeting, setGreeting] = useState(messages[0])
 
-    return <div>
-        <h3>{greeting}! Thank you for visiting!</h3>
-        <button onClick={() => setGreeting(randomMessage())}>
+    return <div className="sm:text-center p-4">
+        <h3 className="font-bold mb-3 text-2xl">{greeting}! Thank you for visiting!</h3>
+        <button className="rounded-full border-2 py-1 px-2 font-bold border-blue-500" onClick={() => setGreeting(randomMessage())}>
             New greeting
         </button>
     </div>
