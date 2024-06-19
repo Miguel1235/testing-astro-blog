a95b18d791887bdddd2f413b3d96e8bf698c2fbb
Miguel <migueldelcorso@gmail.com>
2024-06-19 18:45:18 -0300

    fixed some styles in the site

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -7,7 +7,7 @@ const Greeting = ({messages}) => {
 
     return <div className="sm:text-center p-4">
         <h3 className="font-bold mb-3 text-2xl">{greeting}! Thank you for visiting!</h3>
-        <button className="rounded-full border-2 py-1 px-4 m-4 font-bold border-blue-500 hover:bg-blue-900 hover:text-slate-400  hover:border-slate-400" onClick={() => setGreeting(randomMessage())}>
+        <button className="rounded-full border-2 py-1 px-4 my-4 sm:m-4 font-bold border-blue-500 hover:bg-blue-900 hover:text-slate-400  hover:border-slate-400" onClick={() => setGreeting(randomMessage())}>
             New greeting
         </button>
     </div>
